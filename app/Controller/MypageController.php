c3765396cd1e4737fbb3c75a89ec6b54b38a7bc2
Shun <shun.ozawa34@gmail.com>
2014-01-13 06:02:24 +0900

    Modelがわかりませんバグ

--- app/Controller/MypageController.php
+++ app/Controller/MypageController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // MypageController.php
 
 App::uses('AppController', 'Controller');
@@ -7,6 +7,12 @@ class MypageController extends AppController
     public $name = 'Mypage';
 
     public function index(){
+    	//これ
+    	$user = $this -> User -> getUserById('12345678');
+
+    	//テスト用
+    	$this -> set("user",array("ワインレッド","winered@gmail.com","医療法人泰玄会"));
+    	$this -> set("kuchikomi",array(array("医療法人泰玄会","2014-01-13","とてもいい病院です。職場の人が優しい"),array("みどりグループ","2014-01-12","職場内が常に険悪なムードです")));
     }
 }
 
--- app/View/Mypage/index.ctp
+++ app/View/Mypage/index.ctp
@@ -1,1 +1,19 @@
-mypage
+<div id="userMenu">
+	<?php echo $user[0] ?>
+	<ul>
+		<li>email： <?php echo $user[1] ?></li>
+		<li>passsword： 伏せてあります</li>
+		<li>勤務病院： <?php echo $user[2]  ?></li>
+	</ul>
+</div>
+<div id="history">
+	<ul id="history">
+		<?php
+		foreach ($kuchikomi as $datas) {
+			echo "<li>{$datas[1]}</li>";
+			echo "<li>{$datas[0]}</li>";
+			echo "<li>{$datas[2]}</li>";
+		}
+		 ?>
+	</ul>
+</div>
