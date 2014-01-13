1d22e0abe6dfe555c99f9ba8e2c7d748244e6289
Yutaka Kohada <y.kohada@gmail.com>
2014-01-13 10:18:08 +0900

    show hospital data

--- app/Controller/HospitalController.php
+++ app/Controller/HospitalController.php
@@ -5,19 +5,26 @@ App::uses('AppController', 'Controller');
 class HospitalController extends AppController
 {
     public $name = 'Hospital';
-    public $uses = array('Hospital','Zip');
+    public $uses = array('Hospital','Zip','Station','Facility');
 
     public function index(){
     	$this->redirect('/');
     }
     public function info($id){
     	$results = $this->Hospital->getHospitalInfoById($id);
-    	$zip_code = $results[0]['Hospital']['zip_code'];
-    	$address = $this->Zip->getZipNameById($zip_code);
-    	$address = $address[0]['Zip']['pref_name'].$address[0]['Zip']['ward_name'];
+    	$address = $this->Zip->getZipNameById($results[0]['Hospital']['zip_code']);
+    	$title = $results[0]['Hospital']['company_name'] . " " . $results[0]['Hospital']['hospital_name'];
+    	$address = $address[0]['Zip']['pref_name'] . $address[0]['Zip']['ward_name'];
     	$address = $address.$results[0]['Hospital']['address'];
+    	$station = $this->Station->getStationNameById($results[0]['Hospital']['station_g_cd']);
+    	$station = $station[0]['Station']['line_name'] . " " . $station[0]['Station']['station_name'];
+    	$facility = $this->Facility->getFacilityNameById($results[0]['Hospital']['facility_form_id']);
+    	$facility = $facility[0]['Facility']['disp_name'];
     	$this->set('results',$results);
+    	$this->set('title',$title);
     	$this->set('address',$address);
+    	$this->set('station',$station);
+    	$this->set('facility',$facility);
     }
 }
 
--- app/View/Hospital/info.ctp
+++ app/View/Hospital/info.ctp
@@ -1,9 +1,21 @@
 <div id= "hospitalinfo">
+	<div id="title">
+		<h2><?php echo $title;?></h2>
+	</div>
 	<div id="boxinfo">
 		<table>
 			<tbody>
 				<tr>
-					<?php echo $address;?>
+					<th>住所</th>
+					<td><?php echo $address;?></td>
+				</tr>
+				<tr>
+					<th>交通情報</th>
+					<td><?php echo $station;?></td>
+				</tr>
+				<tr>
+					<th>施設形態</th>
+					<td><?php echo $facility;?></td>
 				</tr>
 			</tbody>
 		</table>
